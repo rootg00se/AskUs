9ee2ae628c251b74f16d8dc076473f45e6a708c6
yusif641 <gorc141408@gmail.com>
2026-01-01 15:41:55 +0300

    frontend setup

---
+++ askus_client/src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/shared/components/ui";
+import type React from "react";
+
+const App: React.FC = () => {
+    return (
+        <div className="wrap">
+            <Button>Some button</Button>
+        </div>
+    );
+};
+
+export default App;
